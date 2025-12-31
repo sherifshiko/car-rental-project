bbe4b51b47f2b71a16388e38b307e7de293554f1
sherifshiko <szshefo@gmail.com>
2025-12-30 18:55:53 -0800

    frist commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
@@ -11,6 +10,8 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    
   ],
+  build: {
+    outDir: 'dist',
+  },
 })
