c897e0592dbce08806d59ad8fb38cd0d04e2907a
sherifshiko <szshefo@gmail.com>
2026-01-05 20:31:29 -0800

    repair home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
       <section className={` bg-light-tart dark:bg-dark-ogin my-5`}>
         <h1 className="text-center m-4 font-bold text-4xl text-maroon dark:text-white">Home pages</h1>
 
-        <div className="grid-cols-3 grid gap-3">
+        <div className="grid-cols-1 md:grid-cols-3 grid gap-3">
           {cars&&cars.map(car=><CardCar key={car.id} cardData={car} />)}
         </div>
       </section>
