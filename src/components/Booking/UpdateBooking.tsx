7cd47078b610a96a7f1c91303828630d8871cb28
sherifshiko <szshefo@gmail.com>
2026-01-19 09:53:12 -0800

    repair

--- src/components/Booking/UpdateBooking.tsx
+++ src/components/Booking/UpdateBooking.tsx
@@ -13,8 +13,6 @@ interface CarData {
 const bookingSchema = Yup.object().shape({
   start_date: Yup.string().required('Start date is required'),
   end_date: Yup.string().required('End date is required'),
-  pickup_location: Yup.string().required('Pickup location is required'),
-  dropoff_location: Yup.string().required('Dropoff location is required'),
   total_price: Yup.string().required('Total price is required'),
   status: Yup.string().required('Status is required')
 });
@@ -68,6 +66,8 @@ const UpdateBooking: React.FC = () => {
     })
       .then(response => {
         setListBookings(listBookings.map(booking => booking.id === selectedBooking?.id ? response.data : booking));
+        alert('Booking updated successfully!')
+        setSelectedBooking(null)
         console.log(response);
       })
       .catch(error => console.error(error));
@@ -89,7 +89,7 @@ const UpdateBooking: React.FC = () => {
               <p className="text-gray-600 font-medium">Start Date: <span className="text-amber-800">{booking.start_date}</span></p>
               <p className="text-gray-600 font-medium">End Date: <span className="text-amber-800">{booking.end_date}</span></p>
               <p className="text-gray-600 font-medium">Status: {booking.status}</p>
-              <p className="text-gray-600 font-medium">User: <span className="font-bold">{booking.user.name}</span></p>
+              <p className="text-gray-600 font-medium">User: <span className="font-bold">{booking.user?.name}</span></p>
             </div>
           </div>
         ))}
@@ -97,7 +97,7 @@ const UpdateBooking: React.FC = () => {
       {selectedBooking && (
         <div className="fixed inset-0 bg-black/75 bg-opacity-50 flex justify-center items-center z-10">
           <div className="bg-gray-500 p-5 rounded-lg w-1/2 h-4/5 overflow-y-auto relative mt-40">
-            <button className="absolute top-2 right-2 bg-red-500 text-white px-2 py-1 rounded" onClick={() => setSelectedBooking(null)}>X</button>
+            <button className="absolute top-2 right-2 bg-red-500 text-white px-2 py-1 rounded cursor-pointer" onClick={() => setSelectedBooking(null)}>X</button>
             <Formik initialValues={selectedBooking} validationSchema={bookingSchema} onSubmit={handleSubmit} enableReinitialize={true} >
               {({ values, handleBlur, handleChange, errors, touched, handleSubmit }) => (
                 <form className="w-4/5" onSubmit={handleSubmit}>
@@ -105,18 +105,12 @@ const UpdateBooking: React.FC = () => {
                     <input onBlur={handleBlur} onChange={handleChange} value={values.start_date} name="start_date" type="text" placeholder="Enter Start Date" className={`rounded-lg border-2 focus:outline-none focus:border-blue-700 p-2 w-full mb-3 ${errors.start_date && touched.start_date && 'border-red-700'}`} />
                     {errors.start_date && touched.start_date && errors.start_date ? <div className="text-red-600">{errors.start_date}</div> : null}
                   </div>
+
                   <div className="mb-3">
                     <input onBlur={handleBlur} onChange={handleChange} value={values.end_date} name="end_date" type="text" placeholder="Enter End Date" className={`rounded-lg border-2 focus:outline-none focus:border-blue-700 p-2 w-full mb-3 ${errors.end_date && touched.end_date && 'border-red-700'}`} />
                     {errors.end_date && touched.end_date && errors.end_date ? <div className="text-red-600">{errors.end_date}</div> : null}
                   </div>
-                  <div className="mb-3">
-                    <input onBlur={handleBlur} onChange={handleChange} value={values.pickup_location ?? ''} name="pickup_location" type="text" placeholder="Enter Pickup Location" className={`rounded-lg border-2 focus:outline-none focus:border-blue-700 p-2 w-full mb-3 ${errors.pickup_location && touched.pickup_location && 'border-red-700'}`} />
-                    {errors.pickup_location && touched.pickup_location && errors.pickup_location ? <div className="text-red-600">{errors.pickup_location}</div> : null}
-                  </div>
-                  <div className="mb-3">
-                    <input onBlur={handleBlur} onChange={handleChange} value={values.dropoff_location ?? ''} name="dropoff_location" type="text" placeholder="Enter Dropoff Location" className={`rounded-lg border-2 focus:outline-none focus:border-blue-700 p-2 w-full mb-3 ${errors.dropoff_location && touched.dropoff_location && 'border-red-700'}`} />
-                    {errors.dropoff_location && touched.dropoff_location && errors.dropoff_location ? <div className="text-red-600">{errors.dropoff_location}</div> : null}
-                  </div>
+
                   <div className="mb-3">
                     <input onBlur={handleBlur} onChange={handleChange} value={values.total_price} name="total_price" type="text" placeholder="Enter Total Price" className={`rounded-lg border-2 focus:outline-none focus:border-blue-700 p-2 w-full mb-3 ${errors.total_price && touched.total_price && 'border-red-700'}`} />
                     {errors.total_price && touched.total_price && errors.total_price ? <div className="text-red-600">{errors.total_price}</div> : null}
@@ -126,7 +120,7 @@ const UpdateBooking: React.FC = () => {
                     {errors.status && touched.status && errors.status ? <div className="text-red-600">{errors.status}</div> : null}
                   </div>
                   <div className="text-center mt-3 ">
-                    <button type="submit" className="bg-green-500 rounded-xl px-5 py-2">Update Booking</button>
+                    <button type="submit" className="bg-green-500 rounded-xl px-5 py-2 cursor-pointer">Update Booking</button>
                   </div>
                 </form>
               )}
