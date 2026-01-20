e1f7fc1b0cd8a23cbb01f1e5890f4b4cde8cdc18
sherifshiko <szshefo@gmail.com>
2026-01-19 16:53:43 -0800

    repair

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -13,5 +14,6 @@ export default defineConfig({
   ],
   build: {
     outDir: 'dist',
+    chunkSizeWarningLimit: 1000, 
   },
 })
